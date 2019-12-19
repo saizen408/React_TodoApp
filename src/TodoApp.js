99a92c813ca9b32dcc12ab04ef4b7fe082ca1551
Eric Owusu <llkoole7@gmail.com>
2019-12-19 12:09:55 -0800

    Add in Todos Context

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import useTodoState from './hooks/useTodoState';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
 import Typography from '@material-ui/core/Typography';
@@ -7,6 +6,8 @@ import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import { Toolbar } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
+import useTodoState from './hooks/useTodoState';
+import { TodosProvider } from './context/todos.context';
 
 function TodoApp() {
   const initialTodos = [{ id: 1, task: 'Pet a Monkey', completed: false }];
@@ -35,13 +36,15 @@ function TodoApp() {
       </AppBar>
       <Grid container justify="center" style={{ marginTop: '1rem' }}>
         <Grid item xs={11} md={8} lg={4}>
-          <TodoForm addTodo={addTodo} />
-          <TodoList
-            todos={todos}
-            removeTodo={removeTodo}
-            toggleTodo={toggleTodo}
-            editTodo={editTodo}
-          />
+          <TodosProvider>
+            <TodoForm addTodo={addTodo} />
+            <TodoList
+              todos={todos}
+              removeTodo={removeTodo}
+              toggleTodo={toggleTodo}
+              editTodo={editTodo}
+            />
+          </TodosProvider>
         </Grid>
       </Grid>
     </Paper>
